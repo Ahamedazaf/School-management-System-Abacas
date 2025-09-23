bcc1e34c134e9dc5ce60ced67063b9e4481a5222
Shifkan04 <Shifkanm04@gmail.com>
2025-09-23 11:45:11 +0530

    default setting current year

--- resources/views/pages/support_team/payments/index.blade.php
+++ resources/views/pages/support_team/payments/index.blade.php
@@ -12,6 +12,7 @@
             <form method="post" action="{{ route('payments.select_year') }}">
                 @csrf
                 <div class="row">
+                    
                     <div class="col-md-6 offset-md-3">
                         <div class="row">
                             <div class="col-md-10">
